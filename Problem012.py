4dcfe62c36640bc16c5e5e85078a1592cb77a1e9
Roger Banks <roger_banks@mac.com>
2021-09-22 17:34:09 -0500

    Correct solution.

--- Problem012.py
+++ Problem012.py
@@ -1,7 +1,13 @@
-for j in range(1, 1000):
+import math
+
+for j in range(1, 50000):
     triangle_number = sum(i for i in range(j+1))
-    factors = [k for k in range(1, triangle_number+1) if triangle_number % k == 0]
-    if len(factors) > 120:
-        print(j, triangle_number, len(factors))
+    factors = [k for k in range(1, math.floor(math.sqrt(triangle_number+1))) if triangle_number % k == 0]
+    more_factors = []
+    for factor in factors:
+         more_factors.append(triangle_number/factor)
+    factors = factors + more_factors
+    if len(factors) > 500:
+        print(triangle_number, len(factors))
         break
 print("Done")
