b22d0e4fa13958ee67047256cdf50bafb45328fc
Roger Banks <roger_banks@mac.com>
2021-09-22 10:23:39 -0500

    Create Problem019.py
    Not working yet.

---
+++ Problem019.py
@@ -0,0 +1,42 @@
+def days_in_month(month, year):
+    if month in ['January', 'March', 'May', 'July', 'August', 'Ocober', 'December']:
+        return 31
+    elif month == 'February':
+        if is_leap(year):
+            return 29
+        return 28
+    else:
+        return 30
+
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        leap = True
+    if year % 100 == 0:
+        leap =  False
+    if year % 400 == 0:
+        leap = True
+    return leap
+    
+def is_Sunday(day):
+    if day % 6 == 0:
+        return True
+    return False
+
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+num_days = 0
+count = 0
+for year in range(1900, 2001):
+    for month in months:
+        days = days_in_month(month, year)
+        for day in range(1, days+1):
+            num_days += 1
+            dow = num_days % 7
+            #print(year, month, day, dow)
+            if day == 1 and dow == 0:
+                count += 1
+                print(year, month, day, dow)
+print(count)
